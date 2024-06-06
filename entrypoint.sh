77ea7260be6b2df9951cc35e5f63dfbca1a26385
Mustafa Tayefi <mustafa@tayefi.me>
2024-06-06 19:44:16 +0330

    debug authentication

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Create password file if not exists
 if [ ! -f /etc/apache2/webdav.password ]; then
-    echo "$WEBDAV_USERNAME:$WEBDAV_PASSWORD" > /etc/apache2/webdav.password
+    htpasswd -b -c /etc/apache2/webdav.password "$WEBDAV_USERNAME" "$WEBDAV_PASSWORD"
 fi
 
 # Start Apache in the foreground
